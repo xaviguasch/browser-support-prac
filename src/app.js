8a12e249427f9d542a3f055d16af23f5454b94ff
xaviguasch <xaviguasch@gmail.com>
2019-11-20 10:37:56 +0100

    added feature detection and fallback code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
-const button = document.querySelector('button');
+const button = document.querySelector('button')
+const textParagraph = document.querySelector('p')
 
 button.addEventListener('click', () => {
-  // do something...
-});
+  const text = textParagraph.textContent
+  if (navigator.clipboard) {
+    navigator.clipboard
+      .writeText(text)
+      .then(result => {
+        console.log(result)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  } else {
+    console.log('Feature not available, please copy manually!')
+  }
+})
